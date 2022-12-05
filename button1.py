ac2d1e03cdcc9bda04c0edf37f959117537701e3
ta-mj <kiatae0722@naver.com>
2022-12-05 11:07:41 +0900

    이름 학번 수정

--- button1.py
+++ button1.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 window =Tk()
 
-b1 = Button(window, text="Taewoo Park")
+b1 = Button(window, text="Taewoo Parkk")
 b2 = Button(window, text="60192199")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
